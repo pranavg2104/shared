5aee604576e5b7bbf29e34bcb75fb37d46ddba16
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-02-02 15:39:50 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -24,27 +24,6 @@ if __name__ == "__main__":
   startTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/startDate",auth = HTTPBasicAuth('admin','admin'))
   endTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/finishDate",auth = HTTPBasicAuth('admin','admin'))
   duration = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/statistics/BuildDuration",auth = HTTPBasicAuth('admin','admin'))
-    
-  """finishDate = endTime.text[:8]
-  finishDate = finishDate[:4] + '-' + finishDate[4:6] + '-' + finishDate[6:]
-  print(finishDate)
-  finishTime = endTime.text[9:15]
-  startTime = startTime[:2] + ':' + startTime[2:4] + ':' + startTime[4:] 
-  print(startTime)
-  finishTime = finishTime[:2] + ':' + finishTime[2:4] + ':' + finishTime[4:] 
-  print(finishTime)
-  print(int(duration.text)/(1000*3600))
-  startDate = sys.argv[3]
-  startDate = startDate[:4] + '-' + startDate[4:6] + '-' + startDate[6:]
-  print(startDate)"""
-
-  endTime = endTime.text
-  endTime = endTime[:4] + '-' + endTime[4:6] + '-' + endTime[6:11] + ':' + endTime[11:13] + ':' + endTime[13:]
-  endTime = endTime.replace("T"," ")
-    
-  startTime = startTime.text
-  startTime = startTime[:4] + '-' + startTime[4:6] + '-' + startTime[6:11] + ':' + startTime[11:13] + ':' + startTime[13:]
-  startTime = startTime.replace("T"," ")
 
   print("Start Time is ",startTime)
   print("End Time is ",endTime)
