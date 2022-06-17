4b39abc02c4dcad8e045a12bb75ad2ec9706c526
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-06-17 12:41:11 +0530

    Add files via upload

---
+++ Flask/app.py
@@ -0,0 +1,21 @@
+from flask import Flask,json,jsonify
+#from flask_restful import Api, Resource
+
+app=Flask(__name__)
+  
+@app.route("/")
+def getEmployeeList():
+        # Initialize a employee list
+    employeeList = []
+        # create a instances for filling up employee list
+    for i in range(0,2):
+        empDict = {
+        'firstName': 'Roy',
+        'lastName': 'Augustine'}
+        employeeList.append(empDict)
+        # convert to json data
+        jsonStr = json.dumps(employeeList)
+    return jsonify(Employees=jsonStr)
+
+if __name__=='__main__':
+    app.run(debug=True)
